eba50e22cf8cc6ec59c1327190956e3f75239699
delchev <delchevn@gmail.com>
2014-03-14 15:59:36 +0300

    xss fix

--- com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.runtime/src/main/java/com/sap/dirigible/runtime/filter/XSSUtils.java
+++ com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.runtime/src/main/java/com/sap/dirigible/runtime/filter/XSSUtils.java
@@ -9,7 +9,7 @@ public class XSSUtils {
 	public static String stripXSS(String value) {
 		if (value != null) {
 			value = StringEscapeUtils.escapeHtml(value);
-			value = StringEscapeUtils.escapeJavaScript(value);
+//			value = StringEscapeUtils.escapeJavaScript(value);
 			value = Jsoup.clean(value, Whitelist.none());
 		}
 		return value;
