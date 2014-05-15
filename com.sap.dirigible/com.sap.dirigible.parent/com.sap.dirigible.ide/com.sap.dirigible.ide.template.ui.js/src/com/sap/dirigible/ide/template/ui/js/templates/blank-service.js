09e9be8bd967a82bd79c84273324b6fe39eddc74
Ivan St. Ivanov <ivan_st_ivanov@yahoo.com>
2014-05-15 17:53:01 +0300

    Set the default content type of the blank service to HTML

--- com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.ide/com.sap.dirigible.ide.template.ui.js/src/com/sap/dirigible/ide/template/ui/js/templates/blank-service.js
+++ com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.ide/com.sap.dirigible.ide.template.ui.js/src/com/sap/dirigible/ide/template/ui/js/templates/blank-service.js
@@ -3,5 +3,7 @@ var systemLib = require('system');
 systemLib.println("Hello World!");
 
 response.getWriter().println("Hello World!");
+
+response.setContentType("text/html")
 response.getWriter().flush();
 response.getWriter().close();
