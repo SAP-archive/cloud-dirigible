7b820394e2eced7105a70a56419232b19b1ac07e
ThuF <jordan_p_pavlov@abv.bg>
2014-06-18 17:38:01 +0300

    Fixes #49 Database view: cannot delete multiple tables at once

--- com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.ide/com.sap.dirigible.ide.db.viewer/src/com/sap/dirigible/ide/db/viewer/views/actions/DeleteTableAction.java
+++ com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.ide/com.sap.dirigible.ide.db.viewer/src/com/sap/dirigible/ide/db/viewer/views/actions/DeleteTableAction.java
@@ -18,10 +18,10 @@ package com.sap.dirigible.ide.db.viewer.views.actions;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Iterator;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 
@@ -43,8 +43,7 @@ public class DeleteTableAction extends Action {
 
 	private static final String FAILED_TO_DELETE_TABLE_S = Messages.DeleteTableAction_FAILED_TO_DELETE_TABLE_S;
 
-	private static final Logger logger = Logger
-			.getLogger(DeleteTableAction.class);
+	private static final Logger logger = Logger.getLogger(DeleteTableAction.class);
 
 	private static final String WARNING_THIS_ACTION_WILL_DELETE_THE_TABLE_AND_ALL_OF_ITS_CONTENT_CONTINUE = Messages.DeleteTableAction_WARNING_THIS_ACTION_WILL_DELETE_THE_TABLE_AND_ALL_OF_ITS_CONTENT_CONTINUE;
 
@@ -62,45 +61,51 @@ public class DeleteTableAction extends Action {
 		setToolTipText(WILL_DELETE_THE_TABLE_AND_ITS_CONTENT);
 	}
 
+	@SuppressWarnings("rawtypes")
 	public void run() {
-		ISelection selection = viewer.getSelection();
-		Object obj = ((IStructuredSelection) selection).getFirstElement();
-		if (TreeObject.class.isInstance(obj)) {
-			if (((TreeObject) obj).getTableDefinition() != null) {
-				TableDefinition tableDefinition = ((TreeObject) obj)
-						.getTableDefinition();
-				boolean confirm = MessageDialog
-						.openConfirm(viewer.getControl().getShell(),
-								DELETE_TABLE,
-								WARNING_THIS_ACTION_WILL_DELETE_THE_TABLE_AND_ALL_OF_ITS_CONTENT_CONTINUE);
-				if (!confirm) {
-					return;
-				}
-				TreeParent parent = ((TreeObject) obj).getParent();
-				IDbConnectionFactory connectionFactory = parent
-						.getConnectionFactory();
-				try {
-					deleteTable(tableDefinition, connectionFactory);
-					RefreshViewAction refresh = new RefreshViewAction(viewer,
-							parent);
-					refresh.run();
-				} catch (SQLException e) {
-					showMessage(String.format(FAILED_TO_DELETE_TABLE_S,
-							tableDefinition.getTableName()));
-					logger.error(e.getMessage(), e);
+		IStructuredSelection selection = (IStructuredSelection) viewer.getSelection();
+		TreeParent parent = null;
+
+		for (Iterator nextSelection = selection.iterator(); nextSelection.hasNext();) {
+			Object obj = nextSelection.next();
+			if (TreeObject.class.isInstance(obj)) {
+				if (((TreeObject) obj).getTableDefinition() != null) {
+					TableDefinition tableDefinition = ((TreeObject) obj).getTableDefinition();
+
+					boolean confirm = MessageDialog
+							.openConfirm(
+									viewer.getControl().getShell(),
+									DELETE_TABLE,
+									String.format(
+											WARNING_THIS_ACTION_WILL_DELETE_THE_TABLE_AND_ALL_OF_ITS_CONTENT_CONTINUE,
+											tableDefinition.getTableName()));
+					if (!confirm) {
+						continue;
+					}
+					parent = ((TreeObject) obj).getParent();
+					IDbConnectionFactory connectionFactory = parent.getConnectionFactory();
+					try {
+						deleteTable(tableDefinition, connectionFactory);
+					} catch (SQLException e) {
+						showMessage(String.format(FAILED_TO_DELETE_TABLE_S,
+								tableDefinition.getTableName()));
+						logger.error(e.getMessage(), e);
+					}
 				}
 			}
 		}
-
+		if (parent != null) {
+			RefreshViewAction refresh = new RefreshViewAction(viewer, parent.getChildren()[0]);
+			refresh.run();
+		}
 	}
 
 	protected void showMessage(String message) {
-		MessageDialog.openError(viewer.getControl().getShell(), DATABASE_VIEW,
-				message);
+		MessageDialog.openError(viewer.getControl().getShell(), DATABASE_VIEW, message);
 	}
 
-	private void deleteTable(TableDefinition tableDefinition,
-			IDbConnectionFactory connectionFactory) throws SQLException {
+	private void deleteTable(TableDefinition tableDefinition, IDbConnectionFactory connectionFactory)
+			throws SQLException {
 		Connection connection = connectionFactory.getDatabaseConnection();
 		try {
 			Statement createStatement = connection.createStatement();
