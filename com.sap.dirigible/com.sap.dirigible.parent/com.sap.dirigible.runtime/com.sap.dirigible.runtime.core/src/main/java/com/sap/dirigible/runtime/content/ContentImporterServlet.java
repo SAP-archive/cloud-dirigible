2c5fc52a13f1f50e58552f99438419ce4bca8395
fen4o <martin.vladev@gmail.com>
2014-10-21 17:09:52 +0300

    Support for import of multiple zip files

--- com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.runtime/com.sap.dirigible.runtime.core/src/main/java/com/sap/dirigible/runtime/content/ContentImporterServlet.java
+++ com.sap.dirigible/com.sap.dirigible.parent/com.sap.dirigible.runtime/com.sap.dirigible.runtime.core/src/main/java/com/sap/dirigible/runtime/content/ContentImporterServlet.java
@@ -1,7 +1,15 @@
 package com.sap.dirigible.runtime.content;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,21 +19,44 @@ import javax.servlet.http.HttpServletResponse;
 import com.sap.dirigible.runtime.logger.Logger;
 
 public class ContentImporterServlet extends BaseContentServlet {
-	
+
 	private static final long serialVersionUID = 5844468087553458293L;
-	
-	private static final Logger logger = Logger.getLogger(ContentImporterServlet.class);
-	
+
+	private static final Logger logger = Logger
+			.getLogger(ContentImporterServlet.class);
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		logger.debug("Importing content...");
-		InputStream in = request.getInputStream();
-		importZipAndUpdate(in);
-		logger.debug("Content imported.");
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+		if (isMultipart) {
+			FileItemFactory factory = new DiskFileItemFactory();
+			ServletFileUpload upload = new ServletFileUpload(factory);
+
+			try {
+				List<FileItem> items = upload.parseRequest(request);
+				logger.debug("Importing multiple content...");
+				for (FileItem fileItem : items) {
+					logger.debug("Importing " + fileItem.getFieldName());
+					InputStream in = fileItem.getInputStream();
+					importZipAndUpdate(in);
+					logger.debug("Content imported.");
+				}
+			} catch (FileUploadException e) {
+				logger.error(e.getMessage());
+			}
+		} else {
+			logger.debug("Importing single content...");
+			InputStream in = request.getInputStream();
+			importZipAndUpdate(in);
+			logger.debug("Content imported.");
+
+		}
 	}
-	
+
 }
