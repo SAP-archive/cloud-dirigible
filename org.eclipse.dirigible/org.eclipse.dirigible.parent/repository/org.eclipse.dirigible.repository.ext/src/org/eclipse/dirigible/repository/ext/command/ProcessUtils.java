1e0560fb4138331bb11db29ebe1dd0c8a35accd6
delchev <delchevn@gmail.com>
2015-05-07 18:25:49 +0300

    fixes related to https://dev.eclipse.org/ipzilla/show_bug.cgi?id=9548#c9

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/repository/org.eclipse.dirigible.repository.ext/src/org/eclipse/dirigible/repository/ext/command/ProcessUtils.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/repository/org.eclipse.dirigible.repository.ext/src/org/eclipse/dirigible/repository/ext/command/ProcessUtils.java
@@ -55,8 +55,32 @@ public class ProcessUtils {
 		return process;
 	}
 	
+	
+	
+	
+/** ===================================================================================== */
+/**	The following function has been taken from Apache Ant code base                       */
+/** ===================================================================================== */
+
+	/*
+	 *  Licensed to the Apache Software Foundation (ASF) under one or more
+	 *  contributor license agreements.  See the NOTICE file distributed with
+	 *  this work for additional information regarding copyright ownership.
+	 *  The ASF licenses this file to You under the Apache License, Version 2.0
+	 *  (the "License"); you may not use this file except in compliance with
+	 *  the License.  You may obtain a copy of the License at
+	 *
+	 *      http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 *  Unless required by applicable law or agreed to in writing, software
+	 *  distributed under the License is distributed on an "AS IS" BASIS,
+	 *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 *  See the License for the specific language governing permissions and
+	 *  limitations under the License.
+	 *
+	 */
+	
 		/**
-		 * LIFTED FROM APACHE ANT CODE BASE!
 		 * 
 	     * Crack a command line.
 	     *
@@ -131,5 +155,7 @@ public class ProcessUtils {
 	        final String[] args = new String[list.size()];
 	        return list.toArray(args);
 	}
+
+/** ===================================================================================== */
 
 }
